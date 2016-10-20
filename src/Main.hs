ae3f3e411763a8c303438d721a3b7a618c234722
sectore <info@websector.de>
2016-10-20 14:54:59 +0200

    finished exercise by Sven

--- src/Main.hs
+++ src/Main.hs
@@ -1,9 +1,10 @@
 module Main where
 
-import           Data.List (minimumBy, sort, transpose)
-import           Data.Map  (Map, fromListWith, mapKeys, (!))
-import qualified Data.Map  as Map
-import           Data.Ord  (comparing)
+import           Data.List  (minimumBy, sort, transpose)
+import           Data.Map   (Map, assocs, elems, fromListWith, keys, mapKeys,
+                             (!))
+import           Data.Maybe
+import           Data.Ord   (comparing)
 
 -- A `Point` is a just a `List` of `Double` entries (a Euclidean vector):
 type Point = [Double]
@@ -33,25 +34,46 @@ pointWithMinDist x = minimumBy (comparing $ dist x)
 -- Replaces the centroid (key) in `centroidsMap` with the centroids
 -- computed from the points (value) in `centroidsMap`, thus returning.
 relocate :: Map Point [Point] -> Map Point [Point]
-relocate centroidsMap = mapKeys (\k -> centroid $ centroidsMap ! k) centroidsMap
+relocate centroidsMap = mapKeys (\k -> relocate' k (centroidsMap ! k)) centroidsMap
+  where
+  relocate' :: Point -> [Point] -> Point
+  relocate' c ps = fromMaybe c (centroid ps)
 
 -- mapKeys :: Ord k2 => (k1 -> k2) -> Map k1 a -> Map k2 a
 
--- ??
-centroid :: [Point] -> Point
-centroid = map avg . transpose
+centroid :: [Point] -> Maybe Point
+centroid [] = Nothing
+centroid ps = Just . map avg . transpose $ ps
 -- transpose :: [[a]] -> [[a]]
 
 avg :: Fractional a => [a] -> a
 avg xs = sum xs / fromIntegral (length xs)
 
+kMeansStep :: ([Point], [Point]) -> ([Point], [Point])
+kMeansStep (cs, ps) = (keys map, concat . elems $ map)
+  where
+  map :: Map Point [Point]
+  map = relocate $ assign cs ps
+
+kMeansGenerator :: Int -> [Point] -> [([Point], [Point])]
+kMeansGenerator k points = generator (centroids, points) kMeansStep
+  where
+  centroids = take k points
+
 -- Performs the k-means algorithm
-kmeans :: [Point] -> [Point] -> [Point]
-kmeans centroids points = undefined
+kMeans :: Int -> [Point] -> [Point]
+kMeans k points = undefined
+
+generator :: a -> (a -> a) -> [a]
+generator s f = s : generator (f s) f
+
+points = [ [0, 0], [1, 0], [0, 1], [1, 1] , [7, 5], [9, 6], [8, 7] ]
+
+showAssign :: (Show a, Show b) => (a, b) -> String
+showAssign (x, y) = show x ++ " -> " ++ show y
+
+showMap :: (Show a, Show b) => Map a b -> String
+showMap = unwords . map showAssign . assocs
 
 main :: IO ()
-main = do
-  let points = [ [0, 0], [1, 0], [0,1], [1,1]
-               , [7, 5], [9, 6], [8, 7] ]
-  let centroids = kmeans (take 2 points) points
-  print centroids
+main = putStr $ unlines $ map showMap $ map (uncurry assign) $ take 3 $ kMeansGenerator 2 points
